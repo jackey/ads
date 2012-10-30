9a84b6d013f51476db2515f58afc91f630dcd445
Jackey chen <jziwenchen@gmail.com>
2012-10-31 07:58:21 +0800

    做了初始化的panel设置

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,12 +7,19 @@ Ext.onReady(function () {
 		region: 'north'
 	});
 
+	function homeLinkClickHandler() {
+		console.log(arguments);
+	}
+
 	var menu = Ext.create('Ext.panel.Panel', {
 		layout:'border',
 		width: '30%',
+		margin: '0 5 0 0',
 		height: 800,
 		region: 'west',
-		floating: false,
+		title: '导航',
+		resize: true,
+		collapsible: true,
 		items: [
 			Ext.create('Ext.menu.Menu', {
 			    width: '100%',
@@ -21,15 +28,26 @@ Ext.onReady(function () {
 			    floating: false,  // usually you want this set to True (default)
 			    renderTo: Ext.getBody(),  // usually rendered by it's containing component
 			    items: [{
-			        text: 'regular item 1'
+			        text: '首页',
+			        handler: function () {
+			        	Ext.Msg.alert("Home", 'Clicked');
+			        }
 			    },{
-			        text: 'regular item 2'
+			        text: '发布广告'
 			    },{
-			        text: 'regular item 3'
+			        text: '账户设置'
 			    }]
 			}),
 		]
 	})
+
+	var content = Ext.create('Ext.panel.Panel', {
+		layout: 'border',
+		width: '68%',
+		region: 'center',
+		height: 800,
+		html: 'hello world'
+	});
 
 	Ext.create('Ext.container.Viewport', {
 		layout: 'border',
