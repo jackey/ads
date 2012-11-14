d58e72d0f853d8c676f452cff65bca25658325b1
Jackychen <jziwenchen@gmail.com>
2012-11-14 10:13:09 +0800

    检测下我是否可以push

--- app.js
+++ app.js
@@ -33,5 +33,6 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
+  // hello 
   console.log("Express server listening on port " + app.get('port'));
 });
