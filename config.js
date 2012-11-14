4df18cc7c38164452b3523219cd6a8568934d6f7
Jackychen <jziwenchen@gmail.com>
2012-11-14 17:07:39 +0800

    添加用户登陆/添加新用户界面

--- config.js
+++ config.js
@@ -5,10 +5,11 @@ module.exports = {
 		'logout': 'user_logout',
 		'add_ads': 'add_ads',
 		'my_ads_list/:uid': 'ads_list',
-		'ads_statistics': 'ads_statstics'
+		'ads_statistics': 'ads_statstics',
+		'add_user':'user_add'
 	},
 	port: 3000,
 	post: {
-		
+
 	}
 }
---
+++ routes/user_add.js
@@ -0,0 +1,3 @@
+module.exports = function (req, res) {
+	res.render('user_add', {title: '广告系统|添加新用户'});
+}
